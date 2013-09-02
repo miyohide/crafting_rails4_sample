4e8b26bd7b8c44176c62bc208bc93dcd90f8595b
Hidenori Miyoshi <miyohide@gmail.com>
2013-09-02 23:05:20 +0900

    add pdf delivery test.

---
+++ pdf_renderer/test/dummy/app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+class HomeController < ApplicationController
+   def index
+      respond_to do |format|
+         format.html
+         format.pdf { render pdf: "contents" }
+      end
+   end
+end
+

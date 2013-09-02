e6411ede43a7136129c67cc1a8527613081a9f96
Hidenori Miyoshi <miyohide@gmail.com>
2013-09-02 23:11:07 +0900

    add pdf renderer

--- pdf_renderer/lib/pdf_renderer.rb
+++ pdf_renderer/lib/pdf_renderer.rb
@@ -1,2 +1,12 @@
-module PdfRenderer
+# coding: utf-8
+
+require "prawn"
+
+ActionController::Renderers.add :pdf do |filename, options|
+   pdf = Prawn::Document.new
+   pdf.text render_to_string(options)
+   send_data(pdf.render, filename: "#{filename}.pdf",
+             disposition: "attachment")
 end
+# module PdfRenderer
+# end

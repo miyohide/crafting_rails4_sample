3a6aaf9ea557d3d0616a499e74d53c3738928274
Hidenori Miyoshi <miyohide@gmail.com>
2013-12-02 22:11:24 +0900

    add suffix method

--- mail_form/lib/mail_form/base.rb
+++ mail_form/lib/mail_form/base.rb
@@ -3,6 +3,8 @@ module MailForm
       include ActiveModel::AttributeMethods
       attribute_method_prefix 'clear_'
 
+      attribute_method_suffix '?'
+
       def self.attributes(*names)
          attr_accessor(*names)
 
@@ -12,6 +14,10 @@ module MailForm
       protected
       def clear_attribute(attribute)
          send("#{attribute}=", nil)
+      end
+
+      def attribute?(attribute)
+         send(attribute).present?
       end
    end
 end

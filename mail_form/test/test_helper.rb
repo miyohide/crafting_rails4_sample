24115c797face6c8c5d0da5bae1f5896bad0103a
Hidenori Miyoshi <miyohide@gmail.com>
2014-01-27 21:36:25 +0900

    Add Capybara configuration.

--- mail_form/test/test_helper.rb
+++ mail_form/test/test_helper.rb
@@ -3,6 +3,8 @@ ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
+require "capybara"
+require "capybara/rails"
 
 Rails.backtrace_cleaner.remove_silencers!
 
@@ -13,3 +15,10 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 if ActiveSupport::TestCase.method_defined?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
 end
+
+# Define a bare test case to use with Capybara
+class ActiveSupport::IntegrationCase < ActiveSupport::TestCase
+   include Capybara::DSL
+   include Rails.application.routes.url_helpers
+end
+

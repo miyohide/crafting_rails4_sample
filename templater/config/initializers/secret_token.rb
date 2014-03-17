4aa092e83657a66da6fc348d2fd81001fa37e33a
Hidenori Miyoshi <miyohide@gmail.com>
2014-03-17 22:44:10 +0900

    P41 create templater application.

---
+++ templater/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Templater::Application.config.secret_key_base = '3201a98efdd9e4e6cbd33acf00f0e6a359d2f807793d5e31092aa63b5ca5e4f2fa319d6dbbe4a2aeed804fcbfbca2adf6b16c2c73fea7a34b7e57e32f2822661'

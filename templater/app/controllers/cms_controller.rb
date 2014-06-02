a2b6d868dc42fffa5d07db8a99a45f4d5e4a642b
Hidenori Miyoshi <miyohide@gmail.com>
2014-06-02 22:40:28 +0900

    reimplement CmsController.

--- templater/app/controllers/cms_controller.rb
+++ templater/app/controllers/cms_controller.rb
@@ -1,5 +1,11 @@
-class CmsController < ApplicationController
+module CmsHelper
+end
+
+class CmsController < ActionController::Metal
+   include ActionController::Rendering
+   include AbstractController::Helpers
    prepend_view_path SqlTemplate::Resolver.instance
+   helper CmsHelper
 
    def respond
       render template: params[:page]

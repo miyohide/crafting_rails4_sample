f51f7300e95dbd6951cf270be16aad5abe15cb05
Hidenori Miyoshi <miyohide@gmail.com>
2014-07-07 22:35:03 +0900

    implement string template

--- handlers/lib/handlers.rb
+++ handlers/lib/handlers.rb
@@ -1,5 +1,6 @@
 require "action_view/template"
 ActionView::Template.register_template_handler :rb, lambda { |template| template.source }
+ActionView::Template.register_template_handler :string, lambda { |template| "%Q{#{template.source}}" }
 
 module Handlers
 end

6a0f1c5d7e083ee345c6b9ea1e39544883b29d72
Hidenori Miyoshi <miyohide@gmail.com>
2014-07-07 22:30:42 +0900

    add test for string template handler.

--- handlers/test/integration/rendering_test.rb
+++ handlers/test/integration/rendering_test.rb
@@ -6,5 +6,11 @@ class RenderingTest < ActionDispatch::IntegrationTest
       expected = "This is my first <b>template handler</b>!"
       assert_match expected, response.body
    end
+
+   test ".string template handler" do
+      get "/handlers/string_handler"
+      expected = "Congratulations! You just created another template handler!"
+      assert_match expected, response.body
+   end
 end
 

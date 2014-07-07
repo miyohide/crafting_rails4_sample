b6e8d91e022f168c586614f12853fbca86297a87
Hidenori Miyoshi <miyohide@gmail.com>
2014-07-07 23:02:30 +0900

    implement md template handler

--- handlers/lib/handlers.rb
+++ handlers/lib/handlers.rb
@@ -1,6 +1,9 @@
 require "action_view/template"
+require "rdiscount"
+
 ActionView::Template.register_template_handler :rb, lambda { |template| template.source }
 ActionView::Template.register_template_handler :string, lambda { |template| "%Q{#{template.source}}" }
+ActionView::Template.register_template_handler :md, lambda { |template| "RDiscount.new(#{template.source.inspect}).to_html" }
 
 module Handlers
 end

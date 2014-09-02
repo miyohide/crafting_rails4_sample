6531a27a619058d31168a14a028694aecdaaf638
miyohide <miyohide@gmail.com>
2014-09-02 22:34:21 +0900

    P84. rails plugin new live_assets --full

---
+++ live_assets/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '8aa7bf0652c857432205c9d1dc02fbc1d26615edbc45657ad8b2b5790858448a2e6b37d5376b890f7f87f99438b7eff6a45b6421c87b08e6f9a3637e919b05a5'

8ca2f01cbd2ad502d3b5ca7b712a24d542abafef
miyohide <miyohide@gmail.com>
2014-10-06 22:57:22 +0900

    P94. Add LiveAssetsTest.

--- live_assets/test/live_assets_test.rb
+++ live_assets/test/live_assets_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
+require "fileutils"
 
 class LiveAssetsTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, LiveAssets
-  end
+   setup do
+      FileUtils.mkdir_p "test/tmp"
+   end
+
+   teardown do
+      FileUtils.rm_rf "test/tmp"
+   end
+
+   test "can subscribe to listener events" do
+      l = LiveAssets.start_listener(:reload, ["test/tmp"])
+      subscriber = []
+      LiveAssets.subscribe(subscriber)
+
+      begin
+         while subscriber.empty?
+            File.write("test/tmp/sample", SecureRandom.hex(20))
+         end
+
+         assert_includes subscriber, :reload
+      ensure
+         LiveAssets.unsubscribe(subscriber)
+         l.kill
+      end
+   end
 end
+

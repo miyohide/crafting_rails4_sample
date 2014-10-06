9f36305eda6010c8a28f757520fa1cc344efc749
miyohide <miyohide@gmail.com>
2014-10-06 22:58:21 +0900

    Fix infinity loop.

--- live_assets/lib/live_assets.rb
+++ live_assets/lib/live_assets.rb
@@ -16,9 +16,11 @@ module LiveAssets
 
    def self.start_listener(event, directories)
       Thread.new do
-         Listen.to(*directories, latency: 0.5) do |_modified, _added, _removed|
+         listener = Listen.to(*directories, latency: 0.5) do |_modified, _added, _removed|
             subscribers.each { |s| s << event }
          end
+
+         listener.start
       end
    end
 end

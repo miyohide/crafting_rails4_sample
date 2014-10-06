28141d503957ab6ba7e37480056162ced92ff218
miyohide <miyohide@gmail.com>
2014-10-06 22:20:33 +0900

    P93. watching the filesystem that our requests can subscribe to.

--- live_assets/lib/live_assets.rb
+++ live_assets/lib/live_assets.rb
@@ -1,4 +1,25 @@
 require "live_assets/engine"
+require "thread"
+require "listen"
 
 module LiveAssets
+   mattr_reader :subscribers
+   @@subscribers = []
+
+   def self.subscribe(subscriber)
+      subscribers << subscriber
+   end
+
+   def self.unsubscribe(subscriber)
+      subscribers.delete(subscriber)
+   end
+
+   def self.start_listener(event, directories)
+      Thread.new do
+         Listen.to(*directories, latency: 0.5) do |_modified, _added, _removed|
+            subscribers.each { |s| s << event }
+         end
+      end
+   end
 end
+

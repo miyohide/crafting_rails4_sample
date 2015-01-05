e282208f3238ec8ca0d169d01d232deced8d2e22
miyohide <miyohide@gmail.com>
2015-01-05 23:09:15 +0900

    P114. rails plugin new responders.

---
+++ responders/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'e1404d80b7688055f2560cf20b8b55012a7dba6f5f5dae7b355b968def88b9666f8798921bb6015dc37486ebf8af6682e13b2426ef7f5113e44baa2a7a96af15'

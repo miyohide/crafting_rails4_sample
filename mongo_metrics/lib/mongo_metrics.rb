8e3aed9259550a08338e49f8a0830f8cda1951c2
miyohide <miyohide@gmail.com>
2015-04-21 23:32:10 +0900

    P136. first subscribe to ActiveSupport::Notifications

--- mongo_metrics/lib/mongo_metrics.rb
+++ mongo_metrics/lib/mongo_metrics.rb
@@ -1,5 +1,10 @@
 require "mongoid"
 require "mongo_metrics/engine"
+require "active_support/notifications"
 
 module MongoMetrics
+  EVENT = "process_action.action_controller"
+  ActiveSupport::Notifications.subscribe EVENT do |*args|
+    MongoMetrics::Metric.store!(args)
+  end
 end

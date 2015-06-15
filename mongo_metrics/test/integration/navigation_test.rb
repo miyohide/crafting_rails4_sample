91b2bb523a9b15a44511224c06650afb869fd691
miyohide <miyohide@gmail.com>
2015-06-15 22:28:58 +0900

    P139 Add navigation test.

--- mongo_metrics/test/integration/navigation_test.rb
+++ mongo_metrics/test/integration/navigation_test.rb
@@ -1,10 +1,24 @@
 require 'test_helper'
 
 class NavigationTest < ActionDispatch::IntegrationTest
-  fixtures :all
+  setup { MongoMetrics::Metric.delete_all }
 
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can visualize notifications" do
+    get main_app.home_foo_path
+    get main_app.home_bar_path
+    get main_app.home_baz_path
+
+    get mongo_metrics.root_path
+    assert_match "Path: /home/foo", response.body
+    assert_match "Path: /home/bar", response.body
+    assert_match "Path: /home/baz", response.body
+  end
+
+  test "can destroy notifications" do
+    get main_app.home_foo_path
+    metric = MongoMetrics::Metric.first
+    delete mongo_metrics.metric_path(metric)
+    assert_empty MongoMetrics::Metric.where(id: metric.id)
+  end
 end
 

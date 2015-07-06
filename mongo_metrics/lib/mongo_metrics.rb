431f1e302d07dcb870383e2ddfd6702e1fe195ef
miyohide <miyohide@gmail.com>
2015-07-06 22:15:49 +0900

    P146. change mute! and mute? method.

--- mongo_metrics/lib/mongo_metrics.rb
+++ mongo_metrics/lib/mongo_metrics.rb
@@ -1,10 +1,22 @@
 require "mongoid"
+require "jquery-rails"
 require "mongo_metrics/engine"
 require "active_support/notifications"
 
 module MongoMetrics
   EVENT = "process_action.action_controller"
   ActiveSupport::Notifications.subscribe EVENT do |*args|
-    MongoMetrics::Metric.store!(args)
+    MongoMetrics::Metric.store!(args) unless mute?
+  end
+
+  def self.mute!
+    Thread.current["sql_metrics.mute"] = true
+    yield
+  ensure
+    Thread.current["sql_metrics.mute"] = false
+  end
+
+  def self.mute?
+    Thread.current["sql_metrics.mute"] || false
   end
 end

3ecea527c471a73468a6cc8f896403523d9f5f98
miyohide <miyohide@gmail.com>
2015-07-20 23:41:08 +0900

    P151. CSV streamer test.

--- mongo_metrics/test/integration/navigation_test.rb
+++ mongo_metrics/test/integration/navigation_test.rb
@@ -25,5 +25,11 @@ class NavigationTest < ActionDispatch::IntegrationTest
     get mongo_metrics.root_path
     assert_equal 0, MongoMetrics::Metric.count
   end
-end
 
+  test "exports data to csv" do
+    get main_app.home_foo_path
+    get mongo_metrics.metrics_path(format: :csv)
+
+    assert_match "process_action.action_controller, ", response.body
+  end
+end

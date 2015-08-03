b9249f2aa166d0421ff5df611b6a29bf8f72567a
miyohide <miyohide@gmail.com>
2015-08-03 22:51:13 +0900

    P156. rails new translator.

---
+++ translator/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Translator::Application.config.secret_key_base = '6842b82daf1c100aa8d48ded377b76a96da4535b0522441fb2ad3740f6bac64c89cabd92a2f9011fa53eafeafab170781cff6185d28324df14a0745b26490297'

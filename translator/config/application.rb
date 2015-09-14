a266e0b7d5cf75e376fc273278c43e9b321e657b
miyohide <miyohide@gmail.com>
2015-09-14 22:10:00 +0900

    P162. configure the I18n framework.

--- translator/config/application.rb
+++ translator/config/application.rb
@@ -19,5 +19,7 @@ module Translator
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    require "translator"
+    config.i18n.backend = Translator::Backend.new
   end
 end

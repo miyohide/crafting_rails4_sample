2a6bf31913a02088b8ae71e17ef8aeb388aa8c39
miyohide <miyohide@gmail.com>
2015-10-19 22:29:13 +0900

    P169-170. devise setup.

--- translator/config/routes.rb
+++ translator/config/routes.rb
@@ -1,5 +1,7 @@
 Translator::Application.routes.draw do
   mount Translator::App, at: "/translator"
+
+  root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

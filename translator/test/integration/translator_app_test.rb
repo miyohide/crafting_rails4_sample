5cb466cf530428d54ddcc308dd1d8e9fe4bf0f5d
miyohide <miyohide@gmail.com>
2015-11-02 23:06:07 +0900

    P172. add sign_in method

--- translator/test/integration/translator_app_test.rb
+++ translator/test/integration/translator_app_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class TranslatorAppTest < ActiveSupport::IntegrationCase
-  setup { Translator.reload! }
+  setup { sign_in(admin) }
+
+  def admin
+    @admin ||= Admin.create!(
+      email: "admin_#{Admin.count}@example.org",
+      password: "12345678"
+    )
+  end
+
+  def sign_in(admin)
+    visit "/admins/sign_in"
+    fill_in "Email", with: admin.email
+    fill_in "Password", with: admin.password
+    click_button "Sign in"
+  end
 
   test "can translate messages from a given locale to another" do
     assert_raise I18n::MissingTranslationData do
